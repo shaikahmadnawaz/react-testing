096b1352b7b6177ad0d53d588aa8d513bde4307f
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-17 15:10:12 +0530

    unit testing done

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Counter from "./Counter";
 
 describe(Counter, () => {
@@ -6,5 +6,25 @@ describe(Counter, () => {
     const { getByTestId } = render(<Counter />);
     const countValue = Number(getByTestId("count").textContent);
     expect(countValue).toEqual(0);
+  });
+
+  it("counter should increment by 1 if the increment button is clicked", () => {
+    const { getByTestId, getByRole } = render(<Counter />);
+    const incrementBtn = getByRole("button", { name: "Increment" });
+    const countValue1 = Number(getByTestId("count").textContent);
+    expect(countValue1).toEqual(0);
+    fireEvent.click(incrementBtn);
+    const countValue2 = Number(getByTestId("count").textContent);
+    expect(countValue2).toEqual(1);
+  });
+
+  it("counter should decrement by 1 if the decrement button is clicked", () => {
+    const { getByTestId, getByRole } = render(<Counter />);
+    const decrementBtn = getByRole("button", { name: "Decrement" });
+    const countValue1 = Number(getByTestId("count").textContent);
+    expect(countValue1).toEqual(0);
+    fireEvent.click(decrementBtn);
+    const countValue2 = Number(getByTestId("count").textContent);
+    expect(countValue2).toEqual(-1);
   });
 });
